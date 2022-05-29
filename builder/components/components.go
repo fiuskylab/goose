c3054a79f01e06c52e18ba9e165334d317f4f10a
Rafael <rafael_breno@outlook.com>
2022-05-29 00:28:46 -0300

    feat: Add more methods and renamed interface

--- builder/components/components.go
+++ builder/components/components.go
@@ -2,15 +2,20 @@ package components
 
 import "syscall/js"
 
-// Components contains all base functions
+// Component contains all base functions
 // for a Component.
-type Components interface {
+type Component interface {
 	// Build builds the components
 	Build() error
+	// GetElement returns current Component's element.
+	GetElement() js.Value
 	// SetFather sets the father of
 	// current Component
-	SetFather(Components) Components
+	SetFather(Component) Component
 	// GetFather gets the father of
 	// current Component
 	GetFather() js.Value
+	// AddChildren adds a new Component as a chieldren
+	// to current Component.
+	AddChild(Component) Component
 }
