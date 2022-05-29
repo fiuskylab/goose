dfe7d69baf3d9c80d64261b079163437768a68b4
Rafael <rafael_breno@outlook.com>
2022-05-28 23:03:15 -0300

    feat: Update Componenets interface

--- builder/components/components.go
+++ builder/components/components.go
@@ -1,8 +1,16 @@
 package components
 
+import "syscall/js"
+
 // Components contains all base functions
 // for a Component.
 type Components interface {
+	// Build builds the components
 	Build() error
+	// SetFather sets the father of
+	// current Component
 	SetFather(Components) Components
+	// GetFather gets the father of
+	// current Component
+	GetFather() js.Value
 }
