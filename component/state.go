31bae36e9feed76e4d89a15d8792207d3a4ce9da
Rafael <rafael_breno@outlook.com>
2022-06-04 22:36:39 -0300

    feat: Add new state structure

--- component/state.go
+++ component/state.go
@@ -2,18 +2,44 @@ package component
 
 import "fmt"
 
-type State struct {
-	_        struct{}
+// State controls the state and
+// actions for the Components
+type State[T funcs] struct {
+	// Modified is a channel that
+	// receives struct{}{} every time
+	// an action occurs.
 	Modified chan struct{}
-	Stop     chan struct{}
+	// Stop is a channel that
+	// receives struct{}{} to Stop
+	// the current process.
+	Stop chan struct{}
+	// Func is the func that
+	// will be ran.
+	Func T
 }
 
-func (s *State) Run(f func() error) {
+// funcs is the Type Constraint to
+// receive a limited type of functios.
+type funcs interface {
+	func() error |
+		func(params ...any) error
+}
+
+// Run runs the process for the
+// Component's state
+func (s *State[T]) Run(params ...any) {
 	for {
 		select {
 		case <-s.Modified:
-			if err := f(); err != nil {
-				fmt.Println(err.Error())
+			switch f := any(s.Func).(type) {
+			case func() error:
+				if err := f(); err != nil {
+					fmt.Println(err.Error())
+				}
+			case func(params ...any) error:
+				if err := f(params...); err != nil {
+					fmt.Println(err.Error())
+				}
 			}
 		case <-s.Stop:
 			return
